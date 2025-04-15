b5f59d98d8eba47e2dbb4a703749802568233e6a
kenny <kennethg@singulargenomics.com>
2025-04-15 15:37:00 +0000

    update with properties and logger

--- src/g4x_helpers/models.py
+++ src/g4x_helpers/models.py
@@ -3,6 +3,7 @@ import re
 import json
 import glob
 import glymur
+import logging
 import anndata
 import numpy as np
 import pandas as pd
@@ -12,29 +13,99 @@ from pathlib import Path
 from dataclasses import dataclass, field, asdict, InitVar
 from typing import List, Tuple, Union, Iterator, Iterable, Any, Generator, Optional, Literal
 
+from g4x_helpers.utils import setup_logger
+
 glymur.set_option("lib.num_threads", 16)
 
-@dataclass(kw_only= True)
+@dataclass()
 class G4XOutput():
 
     run_base: Union[Path, str]
+    sample_id: str
+    log_level: InitVar[int] = logging.INFO
 
-    def __post_init__(self):
+    def __post_init__(self, log_level):
         self.run_base = Path(self.run_base)
 
+        ## this only sets up a stream handler, no file handlers
+        _= self.setup_logger(stream_level= log_level, clear_handlers= True)
+
         with open(self.run_base / "run_meta.json", "r") as f:
-            run_meta = json.load(f)
-            for key, value in run_meta.items():
-                setattr(self, key, value)
+            self.run_meta = json.load(f)
         
         if self.transcript_panel:
             transcript_panel = pd.read_csv(self.run_base / "transcript_panel.csv", index_col= 0, header= 0)
-            self.transcript_panel = transcript_panel.to_dict()['panel_type']
-            self.target_genes = list(self.transcript_panel.keys())
+            self.transcript_panel_dict = transcript_panel.to_dict()['panel_type']
+            self.genes = list(self.transcript_panel_dict.keys())
         if self.protein_panel:
             protein_panel = pd.read_csv(self.run_base / "protein_panel.csv", index_col= 0, header= 0)
-            self.protein_panel = protein_panel.to_dict()['panel_type']
-            self.target_proteins = list(self.protein_panel.keys())
+            self.protein_panel_dict = protein_panel.to_dict()['panel_type']
+            self.proteins = list(self.protein_panel_dict.keys())
+
+
+    def __repr__(self):
+        return f"""
+        G4XOutput for {self.sample_id} located at {self.run_base}.
+            contains transcript: {self.includes_transcript}
+            contains protein: {self.includes_protein}
+        """
+
+
+    @property
+    def machine(self) -> str:
+        return self.run_meta.get('machine', None)
+    @property
+    def run_id(self) -> str:
+        return self.run_meta.get('run_id', None)
+    @property
+    def fc(self) -> str:
+        return self.run_meta.get('fc', None)
+    @property
+    def lane(self) -> str:
+        return self.run_meta.get('lane', None)
+    @property
+    def platform(self) -> str:
+        return self.run_meta.get('platform', None)
+    @property
+    def transcript_panel(self) -> dict:
+        return self.run_meta.get('transcript_panel', None)
+    @property
+    def protein_panel(self) -> dict:
+        return self.run_meta.get('protein_panel', None)
+    @property
+    def software(self) -> str:
+        return self.run_meta.get('software', None)
+    @property
+    def software_version(self) -> str:
+        return self.run_meta.get('software_version', None)
+    @property
+    def logger(self) -> logging.Logger:
+      return logging.getLogger(f"{self.sample_id}_G4XOutput")
+    @property
+    def includes_transcript(self) -> bool:
+        return self.transcript_panel is not None
+    @property
+    def includes_protein(self) -> bool:
+        return self.protein_panel is not None
+    
+    
+    def setup_logger(
+        self,
+        *,
+        stream_logger: Optional[bool] = True,
+        stream_level: Optional[int] = logging.INFO,
+        file_logger: Optional[bool] = False,
+        file_level: Optional[int] = logging.INFO,
+        clear_handlers: Optional[bool] = True
+    ) -> None:
+        _= setup_logger(
+            f"{self.sample_id}_G4XOutput",
+            stream_logger= stream_logger,
+            stream_level= stream_level,
+            file_logger= file_logger,
+            file_level= file_level,
+            clear_handlers= clear_handlers
+        )
 
     def load_adata(
         self,
@@ -43,7 +114,7 @@ class G4XOutput():
         load_clustering: Optional[bool] = True
     ) -> anndata.AnnData:
         adata = sc.read_h5ad(self.run_base / "single_cell_data" / "feature_matrix.h5")
-        adata.obs_names = adata.obs["cell_id"]
+        adata.obs_names = adata.obs['cell_id']
         adata.var_names = adata.var['gene_id']
         if remove_nontargeting:
             adata = adata[:, adata.var.query(" probe_type == 'targeting' ").index].copy()
@@ -54,13 +125,15 @@ class G4XOutput():
                 umap_key = '_'.join(sorted([x for x in adata.obs.columns if 'X_umap' in x])[0].split('_')[:-1])
                 adata.obsm['X_umap'] = adata.obs[[f"{umap_key}_1", f"{umap_key}_2"]].to_numpy(dtype= float)
             except Exception as e:
-                print(f"No clustering results found. {e}")
+                self.logger.exception(f"No single-cell clustering results found.")
+                self.logger.exception(e)
+        adata.obs_names = f"{self.sample_id}-" + adata.obs['cell_id'].str.split('-').str[0]
         return adata
 
     def _get_image(self, parent_directory: str, pattern: str) -> np.ndarray:
         img_path = next((self.run_base / parent_directory).glob(pattern), None)
         if img_path is None:
-            print("Image file does not exist.")
+            self.logger.error("Image file does not exist.")
             return None
         if img_path.suffix == ".jp2":
             img = glymur.Jp2k(img_path)[:]
@@ -70,7 +143,7 @@ class G4XOutput():
 
     def load_protein_image(self, protein: str) -> np.ndarray:
         if not self.protein_panel:
-            print("No protein results.")
+            self.logger.error("No protein results.")
             return None
         assert protein in self.protein_panel, print(f"{protein} not in protein panel.")
         return _get_image("protein", f"{protein}.*")
@@ -92,4 +165,4 @@ class G4XOutput():
         df = pl.read_csv(self.run_base / "rna" / "transcript_table.csv.gz")
         if not return_polars:
             df = df.to_pandas()
-        return df
+        return d
---
+++ src/g4x_helpers/utils.py
@@ -0,0 +1,74 @@
+import os
+import logging
+from pathlib import Path
+from typing import List, Tuple, Union, Iterator, Iterable, Any, Generator, Optional, Literal, Callable
+
+
+def setup_logger(
+    logger_name: str,
+    *,
+    stream_logger: Optional[bool] = True,
+    stream_level: Optional[int] = logging.DEBUG,
+    file_logger: Optional[bool] = False,
+    file_level: Optional[int] = logging.DEBUG,
+    file_mode: Optional[str] = 'a',
+    out_dir: Optional[Union[Path, str]] = None,
+    format: Optional[str] = None,
+    clear_handlers: Optional[bool] = False
+) -> logging.Logger:
+    """
+    Sets up a logger with configurable stream and file handlers.
+
+    Parameters
+    ----------
+    logger_name : str
+        Name of the logger.
+    stream_logger : bool, optional
+        Whether to enable logging to the console (default is True).
+    stream_level : int, optional
+        Logging level for the stream handler (default is logging.DEBUG).
+    file_logger : bool, optional
+        Whether to enable logging to a file (default is False).
+    file_level : int, optional
+        Logging level for the file handler (default is logging.DEBUG).
+    file_mode : str, optional
+        File mode for the log file. Common options: 'a' for append, 'w' for overwrite (default is 'a').
+    out_dir : Path or str, optional
+        Directory where the log file will be saved. Required if `file_logger` is True.
+    format : str, optional
+        Custom log message format. If not provided, a default format will be used.
+    clear_handlers : bool, optional
+        Whether to clear existing handlers from the logger before adding new ones (default is False).
+
+    Returns
+    -------
+    logging.Logger
+        Configured logger instance.
+    
+    """
+    
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
+    if format is None:
+        format = '[%(asctime)s %(levelname)s  %(funcName)s PID=%(process)d] %(message)s'
+    formatter = logging.Formatter(format)
+
+    ## optionally clear existing handlers
+    if clear_handlers:
+        logger.handlers.clear()
+
+    if stream_logger:
+        h = logging.StreamHandler()
+        h.setLevel(stream_level)
+        h.setFormatter(formatter)
+        logger.addHandler(h)
+
+    if file_logger:
+        assert out_dir is not None, "out_dir must be provided if file_logger is True"
+        os.makedirs(out_dir, exist_ok= True)
+        fh = logging.FileHandler(f'{out_dir}/{logger_name}.log', mode= file_mode)
+        fh.setLevel(file_level)
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+
+    return logger
