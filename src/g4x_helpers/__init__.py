df840827a13e4be6eed65b2f328f3c4cff0d9a70
floraths <flo.raths@singulargenomics.com>
2025-07-09 20:37:40 -0700

    execute pre-commit on all files

--- src/g4x_helpers/__init__.py
+++ src/g4x_helpers/__init__.py
@@ -1,8 +1,8 @@
 from importlib.metadata import PackageNotFoundError, version
 
 try:
-    __version__ = version("g4x_helpers")
+    __version__ = version('g4x_helpers')
 except PackageNotFoundError:
-    __version__ = "unknown"
+    __version__ = 'unknown'
 
 from g4x_helpers.models import G4Xoutput as G4Xoutput
