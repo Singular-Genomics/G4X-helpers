9c1f243f43402b2aab138ff2fc1b2484106a959a
floRaths <raths.f@gmail.com>
2025-10-14 00:07:58 -0700

    help messages and output clarification

--- src/g4x_helpers/cli.py
+++ src/g4x_helpers/cli.py
@@ -1,3 +1,6 @@
+# import time as _time
+# _CLI_T0 = _time.perf_counter()
+
 import inspect
 import textwrap
 
@@ -6,7 +9,7 @@ import rich_click as click
 from . import __version__, utils
 from . import main_features as main
 
-# rich_click.rich_click.THEME = 'modern'
+# click.rich_click.THEME = 'modern'
 click.rich_click.MAX_WIDTH = 100
 click.rich_click.COMMANDS_PANEL_TITLE = 'commands'
 click.rich_click.OPTIONS_PANEL_TITLE = 'options'
@@ -27,7 +30,7 @@ click.rich_click.STYLE_OPTIONS_PANEL_BORDER = 'dim'
 click.rich_click.STYLE_COMMANDS_PANEL_BORDER = 'dim'
 # click.rich_click.COMMANDS_BEFORE_OPTIONS = True
 
-click.rich_click.ARGUMENTS_PANEL_TITLE = 'input/output'
+click.rich_click.ARGUMENTS_PANEL_TITLE = 'input'
 
 click.rich_click.COMMAND_GROUPS = {
     'g4x-helpers': [
@@ -36,12 +39,34 @@ click.rich_click.COMMAND_GROUPS = {
     ],
 }
 
+# click.rich_click.OPTION_GROUPS = {
+#     'g4x-helpers': [
+#         {
+#             'name': 'foo',  #
+#             'options': ['--input', '--out-dir'],
+#         }
+#     ]
+# }
+
 CLI_HELP = 'Helper models and post-processing tools for G4X-data\n\ndocs.singulargenomics.com'
-RESEG_HELP = 'Reprocess G4X-output with a new segmentation'
+
+RESEG_HELP = (
+    'Reprocess G4X-output with a new segmentation\n\n'
+    'Takes custom cell segmentation labels as input and re-assigns transcripts and protein signals to those cells.'
+)
+
 REDMX_HELP = 'Reprocess G4X-output with a new transcript manifest'
-UDBIN_HELP = 'Update G4X-viewer .bin file with new metadata'
-NWBIN_HELP = 'Generate G4X-viewer .bin files from sample output'
-TARVW_HELP = 'Package G4X-viewer folder for distribution'
+
+UDBIN_HELP = 'Update existing G4X-viewer .bin file with new metadata'
+
+NWBIN_HELP = 'Create new G4X-viewer .bin file from sample directory'
+
+TARVW_HELP = (
+    'Package G4X-viewer folder for distribution\n\n'
+    'Creates a .tar archive of the G4X_DATA/g4x_viewer folder for easy upload and sharing.\n\n'
+    'Archive file name: {SAMPLE_ID}_g4x_viewer.tar\n\n'
+    'If no OUT_DIR is specified via g4x-helpers top-level command, the archive will be created in the G4X_DATA directory.'
+)
 
 
 # region cli
@@ -52,26 +77,24 @@ TARVW_HELP = 'Package G4X-viewer folder for distribution'
     help=CLI_HELP,
 )
 @click.argument(
-    'sample-dir',
-    required=False,
+    'g4x-data',  #
+    required=True,
     type=click.Path(exists=True, file_okay=False),
-    # default='.',
-    help='Path to G4X sample output folder passed to commands.',
-    # panel='input/output',
+    help='Directory containing G4X-data for a single sample',
+    # panel='input',
 )
 @click.argument(
     'out-dir',
-    required=False,
-    type=click.Path(exists=False, file_okay=False, dir_okay=True, writable=True),
-    default='./g4x_helpers',
-    help='Output directory used by downstream commands.',
-    # panel='input/output',
+    required=True,
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=True),
+    help='Command output directory (update/override G4X_DIR if not provided)',
+    # panel='options [input/output]',
 )
 @click.option(
     '--sample-id',  #
     required=False,
     type=str,
-    help='Sample ID (optional).',
+    help='Sample ID (optional, used for naming outputs)',
 )
 @click.option(
     '-t',
@@ -80,7 +103,7 @@ TARVW_HELP = 'Package G4X-viewer folder for distribution'
     type=int,
     default=utils.DEFAULT_THREADS,
     show_default=True,
-    help='Number of threads to use for processing.',
+    help='Number of threads to use for processing',
 )
 @click.option(
     '-v',
@@ -89,78 +112,89 @@ TARVW_HELP = 'Package G4X-viewer folder for distribution'
     type=int,
     default=1,
     show_default=True,
-    help='Set logging level WARNING (0), INFO (1), or DEBUG (2).',
+    help='Console logging level (0, 1, 2)',
 )
 @click.option(
     '--version',
     is_flag=True,
     default=False,
-    help='Display g4x-helpers version.',
+    help='Display g4x-helpers version',
 )
 @click.pass_context
 # @click.option('-v', '--verbose', count=True, help="Increase verbosity")
-def cli(ctx, sample_dir, out_dir, sample_id, threads, verbose, version):
+def cli(ctx, g4x_data, out_dir, sample_id, threads, verbose, version):
+    # startup_ms = (_time.perf_counter() - _CLI_T0) * 1000
+    # click.secho(f'[startup] CLI initialized in {startup_ms:.1f} ms', dim=True)
+
     if version:
         click.echo(f'g4x-helpers version: {__version__}')
 
     else:
         ctx.ensure_object(dict)
 
-        # out_dir.mkdir(parents=True, exist_ok=True)
-
-        ctx.obj['sample_dir'] = sample_dir
+        ctx.obj['g4x_data'] = g4x_data
         ctx.obj['out_dir'] = out_dir
         ctx.obj['threads'] = threads
         ctx.obj['verbose'] = verbose
         ctx.obj['version'] = __version__
 
-        if sample_dir:
-            sample = utils.initialize_sample(sample_dir=sample_dir, sample_id=sample_id, n_threads=threads)
+        if g4x_data:
+            sample = utils.initialize_sample(g4x_dir=g4x_data, sample_id=sample_id, n_threads=threads)
             ctx.obj['sample'] = sample
 
-        # No subcommand given → show help
-        if ctx.invoked_subcommand is None and sample_dir:
+        # No subcommand given but G4X_DATA is set → show sample info
+        if ctx.invoked_subcommand is None and g4x_data:
             click.secho('\nG4X-helpers successfully initialized with:\n', bold=True, dim=True)
             click.echo(textwrap.indent(repr(sample), prefix='    '))
             click.secho('please provide a subcommand, use -h for help\n', fg='blue')
 
-        elif ctx.invoked_subcommand is None and not sample_dir:
-            if ctx.invoked_subcommand is None:
-                click.echo(ctx.get_help())
-                ctx.exit()
+        # No subcommand and no options given → show help
+        elif ctx.invoked_subcommand is None and not g4x_data:
+            click.echo(ctx.get_help())
+            ctx.exit()
+
+        elif ctx.invoked_subcommand and g4x_data:
+            if not out_dir:
+                click.secho('!! No output directory specified. Do you want to edit in-place?', fg='blue', bold=True)
+                confirm = click.confirm('Confirm in-place editing', default=False)
+                if confirm:
+                    click.secho('In-place editing confirmed!')
+                else:
+                    click.secho('Operation cancelled by user.', fg='red')
+                    ctx.exit()
 
 
 ############################################################
 # region resegment
 @cli.command(name='resegment', help=RESEG_HELP)
 @click.argument(
-    'segmentation-mask',
+    'cell-labels',
     panel='commands',
     required=True,
     type=click.Path(exists=True, dir_okay=False),
-    help='Path to new segmentation mask. Supported file types: .npy, .npz, .geojson.',
+    help='File containing cell segmentation labels.\n\nsupported file types: [.npy, .npz, .geojson]',
 )
 @click.option(
-    '--segmentation-mask-key',
+    '--labels-key',
     required=False,
     type=str,
     default=None,
-    help='Key in npz/geojson where mask/labels should be taken from (optional).',
+    help='Key/column in npz/geojson where labels should be taken from (optional)',
 )
 @click.pass_context
-def resegment(ctx, segmentation_mask, segmentation_mask_key):
+def resegment(ctx, cell_labels, labels_key):
     try:
         main.resegment(
             g4x_out=ctx.obj['sample'],
-            segmentation_mask=segmentation_mask,
+            cell_labels=cell_labels,
             out_dir=ctx.obj['out_dir'],
-            segmentation_mask_key=segmentation_mask_key,
+            labels_key=labels_key,
             n_threads=ctx.obj['threads'],
             verbose=ctx.obj['verbose'],
         )
     except Exception as e:
         func_name = inspect.currentframe().f_code.co_name
-        utils._fail_message(func_name, e)
+        utils._fail_message(func_name, e, trace_back=True)
 
 
 ############################################################
@@ -278,7 +312,7 @@ def new_bin(ctx):
     '--viewer-dir',
     required=False,
     type=click.Path(exists=True, file_okay=False),
-    help='Path to G4X-viewer folder to tar.',
+    help='(optional) Path to G4X-viewer folder. If set, will tar specified folder instead of the one supplied by G4X_DIR.',
 )
 @click.pass_context
 def tar_viewer(ctx, viewer_dir):
