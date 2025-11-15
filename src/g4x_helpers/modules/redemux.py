fda4a6d73334dd442c51806c703d2960792d9409
floRaths <raths.f@gmail.com>
2025-11-14 22:29:58 -0800

    pkg: collapse modules and workflows

--- src/g4x_helpers/modules/redemux.py
+++ src/g4x_helpers/modules/redemux.py
@@ -1,9 +1,11 @@
 import json
+import logging
 import math
 
 # import re
 import shutil
 from datetime import datetime
+from pathlib import Path
 
 # from pathlib import Path
 from typing import TYPE_CHECKING
@@ -13,6 +15,8 @@ import polars as pl
 from tqdm import tqdm
 
 from .. import utils
+from ..modules import demultiplexing as dmx
+from .decorator import workflow
 
 if TYPE_CHECKING:
     from ..models import G4Xoutput
@@ -34,6 +38,43 @@ primer_read_map = {
 }
 
 PROBE_PATTERN = r'^(.*?)-([ACGT]{2,30})-([^-]+)$'
+
+
+@workflow
+def redemux(
+    g4x_obj: 'G4Xoutput',
+    manifest: Path,
+    out_dir: Path,
+    *,
+    batch_size: int = 1_000_000,
+    logger: logging.Logger,
+) -> None:
+    ## preflight checks
+    logger.info('Validating input paths.')
+    manifest = utils.validate_path(manifest, must_exist=True, is_dir_ok=False, is_file_ok=True)
+    out_dir = utils.validate_path(out_dir, must_exist=False, is_dir_ok=True, is_file_ok=False)
+
+    batch_dir = out_dir / 'diagnostics' / 'batches'
+    batch_dir.mkdir(parents=True, exist_ok=True)
+
+    ## make output directory with symlinked files from original
+    logger.info('Creating output directory and symlinking original files.')
+    utils.symlink_original_files(g4x_obj, out_dir)
+
+    ## update metadata and transcript panel file
+    logger.info('Updating metadata and transcript panel file.')
+    dmx.update_metadata_and_tx_file(g4x_obj, manifest, out_dir)
+
+    ## load the new manifest file that we will demux against
+    logger.info('Loading manifest file.')
+    manifest, probe_dict = dmx.parse_input_manifest(manifest)
+
+    logger.info('Performing re-demuxing.')
+    dmx.batched_demuxing(g4x_obj, manifest, probe_dict, batch_dir, batch_size)
+
+    ## concatenate results into final csv and parquet
+    logger.info('Writing updated transcript table.')
+    dmx.concatenate_and_cleanup(batch_dir, out_dir)
 
 
 def one_hot_encode_str_array(seqs: list[str], seq_len: int) -> np.ndarray:
@@ -121,24 +162,6 @@ def demux(
     )
 
     return reads
-
-
-# def load_manifest(file_path: str | Path) -> tuple[pl.DataFrame, dict]:
-#     manifest = pl.read_csv(file_path)
-#     target_list = manifest['probe_name'].to_list()
-
-#     p = re.compile('-[ACGT]{2,30}')
-#     match = re.findall(pattern=p, string=target_list[0])
-#     if len(match) == 0:
-#         probe_dict = {p: '-'.join(p.split('-')[:-1]) for p in target_list}
-#     else:
-#         probe_dict = {}
-#         for probe in target_list:
-#             match = re.findall(pattern=p, string=probe)
-#             probe_dict[probe] = probe.split(match[0])[0]
-#     probe_dict['UNDETERMINED'] = 'UNDETERMINED'
-
-#     return manifest, probe_dict
 
 
 def parse_input_manifest(file_path: str) -> pl.DataFrame:
