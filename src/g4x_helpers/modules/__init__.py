8f6caea8002e26ca485b41028d4e3ec219fc792c
floRaths <raths.f@gmail.com>
2025-11-24 16:55:30 -0800

    pkg: set up version migration logic

--- src/g4x_helpers/modules/__init__.py
+++ src/g4x_helpers/modules/__init__.py
@@ -1,17 +1,14 @@
-from .bin_operations import new_bin_core
+from .bin_operations import new_bin_core, seg_updater_core
 from .redemux import redemux_core
 from .resegment import resegment_core
-
-# from .seg_intersect import intersect_segmentation
-from .seg_updater import seg_updater
-from .tar_viewer import tar_viewer
+from .tar_viewer import tar_viewer_core
 from .tx_converter import tx_converter_core
 
 __all__ = [
     'new_bin_core',
     'redemux_core',
     'resegment_core',
-    'seg_updater',
-    'tar_viewer',
+    'seg_updater_core',
+    'tar_viewer_core',
     'tx_converter_core',
 ]
