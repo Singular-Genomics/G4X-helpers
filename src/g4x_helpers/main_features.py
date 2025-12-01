0701cf556e638bf74bf0c71f9c6069002d126a23
floRaths <raths.f@gmail.com>
2025-12-01 15:48:01 -0800

    feat: new reseg and migrate main_features

--- src/g4x_helpers/main_features.py
+++ src/g4x_helpers/main_features.py
@@ -5,7 +5,6 @@ from typing import TYPE_CHECKING
 import rich_click as click
 
 from . import __version__, utils
-from . import workflows as wflw
 
 if TYPE_CHECKING:
     from .models import G4Xoutput
@@ -17,7 +16,7 @@ def _base_command(func):
     @functools.wraps(func)
     def wrapper(
         g4x_obj: 'G4Xoutput',
-        out_dir: str,
+        out_dir: str | None = None,
         n_threads: int = utils.DEFAULT_THREADS,
         verbose: int = 1,
         file_logger: bool = True,
@@ -25,6 +24,10 @@ def _base_command(func):
     ):
         func_name = func.__name__
 
+        if func_name != 'migrate':
+            g4x_obj.validate()
+
+        out_dir = g4x_obj.data_dir if out_dir is None else out_dir
         out_dir = utils.validate_path(out_dir, must_exist=False, is_dir_ok=True, is_file_ok=False)
 
         if out_dir != g4x_obj.data_dir:
@@ -74,164 +77,170 @@ def resegment(
     logger: logging.Logger,
     **kwargs,
 ) -> None:
-    from .modules import seg_intersect as seg
+    from .modules import edit_bin, init_bin, segment
 
     cell_labels = utils.validate_path(cell_labels, must_exist=True, is_dir_ok=False, is_file_ok=True)
 
-    labels = seg.try_load_segmentation(
-        cell_labels=cell_labels,
-        expected_shape=g4x_obj.shape,
-        labels_key=labels_key,
-    )
+    logger.info('Loading segmentation mask.')
+    labels = segment.try_load_segmentation(cell_labels=cell_labels, expected_shape=g4x_obj.shape, labels_key=labels_key)
 
-    adata, mask = wflw.intersect_segmentation(
+    segment.apply_segmentation(
         g4x_obj=g4x_obj,
         labels=labels,
         out_dir=out_dir,
+        skip_protein_extraction=True,  ### TODO <<< RMOVE THIS HACK LATER
         logger=logger,
     )
 
-    # TODO this pattern is repeated in several places, refactor into utility function
-    view_dir = out_dir / 'g4x_viewer'
-    view_dir.mkdir(parents=True, exist_ok=True)
+    init_bin.init_bin_file(g4x_obj=g4x_obj, out_dir=out_dir, logger=logger, n_threads=n_threads)
 
-    wflw.seg_converter(
-        adata=adata,
-        seg_mask=mask,
-        out_path=view_dir / f'{g4x_obj.sample_id}.bin',
-        protein_list=[f'{x}_intensity_mean' for x in g4x_obj.proteins],
-        n_threads=n_threads,
-        logger=logger,
-    )
+    bin_file = out_dir / 'g4x_viewer' / f'{g4x_obj.sample_id}_segmentation.bin'
+    edit_bin.edit_bin_file(g4x_obj=g4x_obj, bin_file=bin_file, logger=logger)
+
+
+# @_base_command
+# def redemux(
+#     g4x_obj: 'G4Xoutput',
+#     out_dir: str,
+#     manifest: str,
+#     *,
+#     batch_size: int = 1_000_000,
+#     n_threads: int = 4,
+#     logger: logging.Logger,
+#     **kwargs,
+# ) -> None:
+#     wflw.redemux(
+#         g4x_obj=g4x_obj,
+#         manifest=manifest,
+#         out_dir=out_dir,
+#         batch_size=batch_size,
+#         logger=logger,
+#     )
+
+#     ## now regenerate the secondary files
+#     logger.info('Regenerating downstream files.')
+#     g4x_obj.data_dir = out_dir  ## set base directory to the redemux output folder for downstream steps
+
+#     # resegment with existing segmentation
+#     logger.info('Intersecting with existing cell segmentation.')
+#     adata, mask = wflw.intersect_segmentation(
+#         g4x_obj=g4x_obj,
+#         labels=g4x_obj.load_segmentation(),
+#         out_dir=out_dir,
+#         logger=logger,
+#     )
+
+#     view_dir = out_dir / 'g4x_viewer'
+#     view_dir.mkdir(parents=True, exist_ok=True)
+
+#     wflw.seg_converter(
+#         adata=adata,
+#         seg_mask=mask,
+#         out_path=view_dir / f'{g4x_obj.sample_id}.bin',
+#         protein_list=[f'{x}_intensity_mean' for x in g4x_obj.proteins],
+#         n_threads=n_threads,
+#         logger=logger,
+#     )
+
+#     wflw.tx_converter(
+#         g4x_obj=g4x_obj,
+#         out_path=view_dir / f'{g4x_obj.sample_id}.tar',
+#         n_threads=n_threads,
+#         logger=logger,
+#     )
+
+
+# @_base_command
+# def update_bin(
+#     g4x_obj: 'G4Xoutput',
+#     out_dir: str,
+#     metadata: str,
+#     *,
+#     bin_file: str | None = None,
+#     cellid_key: str | None = None,
+#     cluster_key: str | None = None,
+#     cluster_color_key: str | None = None,
+#     emb_key: str | None = None,
+#     logger: logging.Logger,
+#     **kwargs,
+# ) -> None:
+#     metadata = utils.validate_path(metadata, must_exist=True, is_dir_ok=False, is_file_ok=True)
+
+#     view_dir = out_dir / 'g4x_viewer'
+#     view_dir.mkdir(parents=True, exist_ok=True)
+
+#     wflw.seg_updater(
+#         bin_file=g4x_obj.data_dir / 'g4x_viewer' / f'{g4x_obj.sample_id}.bin' if bin_file is None else bin_file,
+#         metadata_file=metadata,
+#         out_path=view_dir / f'{g4x_obj.sample_id}.bin',
+#         cellid_key=cellid_key,
+#         cluster_key=cluster_key,
+#         cluster_color_key=cluster_color_key,
+#         emb_key=emb_key,
+#         logger=logger,
+#     )
+
+
+# @_base_command
+# def new_bin(
+#     g4x_obj: 'G4Xoutput',  #
+#     out_dir: str,
+#     *,
+#     n_threads: int = 4,
+#     logger: logging.Logger,
+#     **kwargs,
+# ) -> None:
+#     ## set up the data
+#     try:
+#         adata = g4x_obj.load_adata()
+#         emb_key = '_'.join(sorted([x for x in adata.obs.columns if 'X_umap' in x])[0].split('_')[:-1])
+#         cluster_key = sorted([x for x in adata.obs.columns if 'leiden' in x])[0]
+#         logger.info('Successfully loaded adata with clustering information.')
+
+#     except Exception:
+#         adata = g4x_obj.load_adata(load_clustering=False)
+#         emb_key = None
+#         cluster_key = None
+#         logger.info('Clustering information was not found, cell coloring will be random.')
+
+#     view_dir = out_dir / 'g4x_viewer'
+#     view_dir.mkdir(parents=True, exist_ok=True)
+
+#     wflw.seg_converter(
+#         adata=adata,
+#         seg_mask=g4x_obj.load_segmentation(),
+#         out_path=view_dir / f'{g4x_obj.sample_id}.bin',
+#         cluster_key=cluster_key,
+#         emb_key=emb_key,
+#         protein_list=[f'{x}_intensity_mean' for x in g4x_obj.proteins],
+#         n_threads=n_threads,
+#         logger=logger,
+#     )
+
+
+# @_base_command
+# def tar_viewer(
+#     g4x_obj: 'G4Xoutput',  #
+#     out_dir: str | None = None,
+#     *,
+#     logger: logging.Logger,
+#     **kwargs,
+# ) -> None:
+#     viewer_dir = g4x_obj.data_dir / 'g4x_viewer'
+#     wflw.tar_viewer(viewer_dir=viewer_dir, out_dir=out_dir, logger=logger)
 
 
 @_base_command
-def redemux(
+def migrate(
     g4x_obj: 'G4Xoutput',
-    out_dir: str,
-    manifest: str,
-    *,
-    batch_size: int = 1_000_000,
-    n_threads: int = 4,
-    logger: logging.Logger,
-    **kwargs,
-) -> None:
-    wflw.redemux(
-        g4x_obj=g4x_obj,
-        manifest=manifest,
-        out_dir=out_dir,
-        batch_size=batch_size,
-        logger=logger,
-    )
-
-    ## now regenerate the secondary files
-    logger.info('Regenerating downstream files.')
-    g4x_obj.data_dir = out_dir  ## set base directory to the redemux output folder for downstream steps
-
-    # resegment with existing segmentation
-    logger.info('Intersecting with existing cell segmentation.')
-    adata, mask = wflw.intersect_segmentation(
-        g4x_obj=g4x_obj,
-        labels=g4x_obj.load_segmentation(),
-        out_dir=out_dir,
-        logger=logger,
-    )
-
-    view_dir = out_dir / 'g4x_viewer'
-    view_dir.mkdir(parents=True, exist_ok=True)
-
-    wflw.seg_converter(
-        adata=adata,
-        seg_mask=mask,
-        out_path=view_dir / f'{g4x_obj.sample_id}.bin',
-        protein_list=[f'{x}_intensity_mean' for x in g4x_obj.proteins],
-        n_threads=n_threads,
-        logger=logger,
-    )
-
-    wflw.tx_converter(
-        g4x_obj=g4x_obj,
-        out_path=view_dir / f'{g4x_obj.sample_id}.tar',
-        n_threads=n_threads,
-        logger=logger,
-    )
-
-
-@_base_command
-def update_bin(
-    g4x_obj: 'G4Xoutput',
-    out_dir: str,
-    metadata: str,
-    *,
-    bin_file: str | None = None,
-    cellid_key: str | None = None,
-    cluster_key: str | None = None,
-    cluster_color_key: str | None = None,
-    emb_key: str | None = None,
-    logger: logging.Logger,
-    **kwargs,
-) -> None:
-    metadata = utils.validate_path(metadata, must_exist=True, is_dir_ok=False, is_file_ok=True)
-
-    view_dir = out_dir / 'g4x_viewer'
-    view_dir.mkdir(parents=True, exist_ok=True)
-
-    wflw.seg_updater(
-        bin_file=g4x_obj.data_dir / 'g4x_viewer' / f'{g4x_obj.sample_id}.bin' if bin_file is None else bin_file,
-        metadata_file=metadata,
-        out_path=view_dir / f'{g4x_obj.sample_id}.bin',
-        cellid_key=cellid_key,
-        cluster_key=cluster_key,
-        cluster_color_key=cluster_color_key,
-        emb_key=emb_key,
-        logger=logger,
-    )
-
-
-@_base_command
-def new_bin(
-    g4x_obj: 'G4Xoutput',  #
-    out_dir: str,
-    *,
-    n_threads: int = 4,
-    logger: logging.Logger,
-    **kwargs,
-) -> None:
-    ## set up the data
-    try:
-        adata = g4x_obj.load_adata()
-        emb_key = '_'.join(sorted([x for x in adata.obs.columns if 'X_umap' in x])[0].split('_')[:-1])
-        cluster_key = sorted([x for x in adata.obs.columns if 'leiden' in x])[0]
-        logger.info('Successfully loaded adata with clustering information.')
-
-    except Exception:
-        adata = g4x_obj.load_adata(load_clustering=False)
-        emb_key = None
-        cluster_key = None
-        logger.info('Clustering information was not found, cell coloring will be random.')
-
-    view_dir = out_dir / 'g4x_viewer'
-    view_dir.mkdir(parents=True, exist_ok=True)
-
-    wflw.seg_converter(
-        adata=adata,
-        seg_mask=g4x_obj.load_segmentation(),
-        out_path=view_dir / f'{g4x_obj.sample_id}.bin',
-        cluster_key=cluster_key,
-        emb_key=emb_key,
-        protein_list=[f'{x}_intensity_mean' for x in g4x_obj.proteins],
-        n_threads=n_threads,
-        logger=logger,
-    )
-
-
-@_base_command
-def tar_viewer(
-    g4x_obj: 'G4Xoutput',  #
-    out_dir: str | None = None,
+    restore: bool = False,
     *,
     logger: logging.Logger,
     **kwargs,
 ) -> None:
-    viewer_dir = g4x_obj.data_dir / 'g4x_viewer'
-    wflw.tar_viewer(viewer_dir=viewer_dir, out_dir=out_dir, logger=logger)
+    from .schemas import migration
+
+    if restore:
+        migration.restore_backup(sample_base=g4x_obj.data_dir, sample_id=g4x_obj.sample_id, logger=logger)
+    else:
+        migration.migrate_g4x_data(data_dir=g4x_obj.data_dir, sample_id=g4x_obj.sample_id, logger=logger)
