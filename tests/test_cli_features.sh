3dd0ae6b41b327512f4abe9b6d9b580920c47d53
floRaths <raths.f@gmail.com>
2025-12-10 21:25:52 -0800

    test in-place commands too

--- tests/test_cli_features.sh
+++ tests/test_cli_features.sh
@@ -12,3 +12,12 @@ uv run g4x-helpers -v 2 new_bin .
 uv run g4x-helpers -v 2 update_bin . --metadata single_cell_data/clustering_umap.csv.gz --cluster-key leiden_0.400 --cellid-key label 
 uv run g4x-helpers -v 2 resegment . --cell-labels segmentation/segmentation_mask.npz --labels-key nuclei_exp 
 uv run g4x-helpers -v 2 redemux . --manifest transcript_panel.csv 
+
+uv run g4x-helpers -v 2 tar_viewer . --in-place
+uv run g4x-helpers -v 2 new_bin . --in-place
+uv run g4x-helpers -v 2 update_bin . --metadata single_cell_data/clustering_umap.csv.gz --cluster-key leiden_0.400 --cellid-key label --in-place
+uv run g4x-helpers -v 2 resegment . --cell-labels segmentation/segmentation_mask.npz --labels-key nuclei_exp --in-place
+uv run g4x-helpers -v 2 redemux . --manifest transcript_panel.csv --in-place
+
+cd ..
+rm -r test_data
