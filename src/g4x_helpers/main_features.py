6b85382d8e71ce8d12b968c01f20a11bd5baffb1
floRaths <raths.f@gmail.com>
2026-04-14 16:40:45 -0700

    implement resegment & redemux main features

--- src/g4x_helpers/main_features.py
+++ src/g4x_helpers/main_features.py
@@ -1,14 +1,15 @@
 import functools
 import logging
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Literal
 
-import rich_click as click
-
-from . import __version__, c, utils
-from .cli.cli_setup import print_k_v
+from . import __version__, c, io
+from . import logging_utils as logut
+from . import utils as ut
 
 if TYPE_CHECKING:
     from .g4x_output import G4Xoutput
+
+LOGGER = logging.getLogger(__name__)
 
 
 def _base_command(func):
@@ -16,139 +17,153 @@ def _base_command(func):
 
     @functools.wraps(func)
     def wrapper(
-        g4x_obj: 'G4Xoutput',
+        smp: 'G4Xoutput',
+        *,
         out_dir: str | None = None,
         n_threads: int = c.DEFAULT_THREADS,
         verbose: int = 1,
-        file_logger: bool = True,
+        compute_backend: Literal['cpu', 'gpu', 'auto'] = 'auto',
+        logger: logging.Logger | None = None,
         **kwargs,
     ):
         func_name = func.__name__
+        out_dir = io.pathval.validate_dir_path(out_dir)
 
-        if func_name not in ('migrate', 'validate'):
-            g4x_obj.validate()
-
-        out_dir = g4x_obj.data_dir if out_dir is None else out_dir
-        out_dir = utils.validate_path(out_dir, must_exist=False, is_dir_ok=True, is_file_ok=False)
-
-        if out_dir != g4x_obj.data_dir:
+        if out_dir != smp.data_dir:
             func_out = out_dir / func_name
-            func_out.mkdir(parents=True, exist_ok=True)
+            if not func_out.exists():
+                func_out.mkdir(parents=True, exist_ok=True)
             out_dir = func_out
 
-        gap = 12
-        click.secho(f'\nStarting: {func_name}\n', bold=True)
-        print_k_v('sample_dir', f'{g4x_obj.data_dir}', gap)
-        print_k_v('out_dir', f'{out_dir}', gap)
-        print_k_v('n_threads', f'{n_threads}', gap)
-        print_k_v('verbosity', f'{verbose}', gap)
-        print_k_v('g4x-helpers', f'v{__version__}', gap)
-        click.echo('')
+        log_dir = out_dir / 'logs'
+        if not log_dir.exists():
+            log_dir.mkdir(parents=True, exist_ok=True)
 
-        log_dir = g4x_obj.data_dir / 'g4x_helpers' / 'logs'
-        log_dir.mkdir(parents=True, exist_ok=True)
-        if not kwargs.get('logger', None):
-            logger = utils.setup_logger(
-                logger_name=func_name, out_dir=log_dir, stream_level=verbose, file_logger=file_logger
+        if logger is None:
+            # TODO enable append time when testing is complete
+            func_log = logut.configure_g4x_logging(
+                level=verbose * 10, file_log=True, out_dir=log_dir, append_time=False, file_mode='w'
             )
-        logger.info(f'Running {func_name} with G4X-helpers v{__version__}')
+        else:
+            func_log = logger
+
+        backend = io.get_backend(compute_backend)
+        compute_eng = f'{backend.kind}'
+        compute_eng += ' (detected)' if compute_backend == 'auto' else ''
+
+        d = {
+            'sample_dir': f'{smp.data_dir}',
+            'out_dir': f'{out_dir}',
+            'verbosity': f'{verbose}',
+            'n_threads': f'{n_threads}',
+            'compute_eng': compute_eng,
+            'g4x-helpers': f'v{__version__}',
+        }
+
+        header = f'Initializing G4X-helpers [{func_name}]\n'
+        msg = ut.pretty_dict_str(d)
+        logut.log_msg_wrapped(header=header, msg=msg, prefix='  ', logger=func_log)
 
         result = func(
-            g4x_obj=g4x_obj,
+            smp=smp,
             out_dir=out_dir,
             n_threads=n_threads,
-            verbose=verbose,
-            logger=logger,
+            compute_backend=backend.kind,
+            # logger=func_log,
             **kwargs,
         )
 
-        click.secho(f'\nCompleted: {func.__name__}', bold=True, fg='green')
+        func_log.info(f'Completed: {func.__name__}')
         return result
 
     return wrapper
 
 
 @_base_command
-def resegment(
-    g4x_obj: 'G4Xoutput',
+def testo(
+    smp: 'G4Xoutput',
     out_dir: str,
-    cell_labels: str,
-    *,
-    labels_key: str | None = None,
-    n_threads: int = c.DEFAULT_THREADS,
-    logger: logging.Logger,
+    compute_backend: Literal['cpu', 'gpu', 'auto'] = 'auto',
+    overwrite: bool = True,
     **kwargs,
-) -> None:
-    from .modules import init_bin, segment
+):
+    log = kwargs.get('logger', LOGGER)
+    log.info('This is a test command.')
+    log.info(f'Output directory: {out_dir}')
+    log.info(f'Compute backend: {compute_backend}')
+    log.info(f'Overwrite: {overwrite}')
 
-    cell_labels = utils.validate_path(cell_labels, must_exist=True, is_dir_ok=False, is_file_ok=True)
 
-    logger.info('Loading segmentation mask.')
-    labels = segment.try_load_segmentation(cell_labels=cell_labels, expected_shape=g4x_obj.shape, labels_key=labels_key)
+@_base_command
+def resegment(
+    smp: 'G4Xoutput',
+    out_dir: str,
+    segmentation_mask: str,
+    mask_key: str | None = None,
+    overwrite: bool = True,
+    show_progress: bool = False,
+    compute_backend: Literal['cpu', 'gpu', 'auto'] = 'auto',
+    **kwargs,
+):
+    from .modules import aggregate, single_cell, viewer
 
-    segment.apply_segmentation(
-        g4x_obj=g4x_obj,
-        labels=labels,
+    log = kwargs.get('logger', LOGGER)
+
+    aggregate.aggregate_cell_data(
+        smp,
+        segmentation_mask=segmentation_mask,
+        mask_key=mask_key,
         out_dir=out_dir,
-        skip_protein_extraction=False,
-        logger=logger,
+        overwrite=overwrite,
+        compute_backend=compute_backend,
+        show_progress=show_progress,
+        logger=log,
+    )
+    single_cell.process_sc_output(
+        smp, out_dir=out_dir, compute_backend=compute_backend, overwrite=overwrite, logger=log
     )
 
-    init_bin.init_bin_file(g4x_obj=g4x_obj, out_dir=out_dir, logger=logger, n_threads=n_threads)
-
-    # TODO tell user to run update_bin afterwards
-
-    # bin_file = out_dir / 'g4x_viewer' / f'{g4x_obj.sample_id}_segmentation.bin'
-    # edit_bin.edit_bin_file(g4x_obj=g4x_obj, bin_file=bin_file, logger=logger)
+    viewer.create_viewer_zarr(smp, out_dir=out_dir, overwrite=overwrite, logger=log)
+    viewer.cells.write_cells(smp, seg_name='g4x-default', overwrite=overwrite, logger=log)
 
 
 @_base_command
 def redemux(
-    g4x_obj: 'G4Xoutput',
+    smp: 'G4Xoutput',
     out_dir: str,
     manifest: str,
-    *,
-    batch_size: int = 1_000_000,
-    n_threads: int = c.DEFAULT_THREADS,
-    logger: logging.Logger,
+    overwrite: bool = True,
+    show_progress: bool = False,
+    compute_backend: Literal['cpu', 'gpu', 'auto'] = 'auto',
     **kwargs,
-) -> None:
-    from g4x_helpers.modules import demux, init_bin, segment, transcript_tar
+):
+    from .modules import aggregate, demux, single_cell, viewer
+
+    log = kwargs.get('logger', LOGGER)
 
     demux.demux_raw_features(
-        g4x_obj=g4x_obj,
+        smp,
         manifest=manifest,
         out_dir=out_dir,
-        batch_size=batch_size,
-        logger=logger,
+        overwrite=overwrite,
+        show_progress=show_progress,
+        logger=log,
     )
 
-    labels = g4x_obj.load_segmentation()
-
-    segment.apply_segmentation(
-        g4x_obj=g4x_obj,
-        labels=labels,
-        tx_table=out_dir / 'rna' / 'transcript_table.csv.gz',
+    aggregate.aggregate_cell_data(
+        smp,
         out_dir=out_dir,
-        skip_protein_extraction=False,
-        create_source=False,
-        logger=logger,
+        overwrite=overwrite,
+        compute_backend=compute_backend,
+        show_progress=show_progress,
+        logger=log,
+    )
+    single_cell.process_sc_output(
+        smp, out_dir=out_dir, compute_backend=compute_backend, overwrite=overwrite, logger=log
     )
 
-    init_bin.init_bin_file(g4x_obj=g4x_obj, out_dir=out_dir, n_threads=n_threads, logger=logger)
-
-    # TODO tell user to run update_bin afterwards
-
-    # bin_file = out_dir / 'g4x_viewer' / f'{g4x_obj.sample_id}_segmentation.bin'
-    # edit_bin.edit_bin_file(g4x_obj=g4x_obj, bin_file=bin_file, logger=logger)
-
-    transcript_tar.create_tx_tarfile(
-        g4x_obj=g4x_obj,
-        tx_table=out_dir / 'rna' / 'transcript_table.csv.gz',
-        out_path=out_dir / 'g4x_viewer' / f'{g4x_obj.sample_id}_transcripts.tar',
-        n_threads=n_threads,
-        logger=logger,
-    )
+    viewer.create_viewer_zarr(smp, out_dir=out_dir, overwrite=overwrite, logger=log)
+    viewer.cells.write_cells(smp, seg_name='g4x-default', overwrite=overwrite, logger=log)
 
 
 @_base_command
@@ -191,3 +206,60 @@ def validate(
         validate.validate_file_schemas(g4x_obj.data_dir, verbose=True)
     except validate.ValidationError as e:
         print('File schema validation failed!: ', e)
+
+
+# def _base_command(func):
+#     """Decorator to apply standard command initialization logic."""
+
+#     @functools.wraps(func)
+#     def wrapper(
+#         g4x_obj: 'G4Xoutput',
+#         out_dir: str | None = None,
+#         n_threads: int = c.DEFAULT_THREADS,
+#         verbose: int = 1,
+#         file_logger: bool = True,
+#         **kwargs,
+#     ):
+#         func_name = func.__name__
+
+#         if func_name not in ('migrate', 'validate'):
+#             g4x_obj.validate()
+
+#         out_dir = g4x_obj.data_dir if out_dir is None else out_dir
+#         out_dir = utils.validate_path(out_dir, must_exist=False, is_dir_ok=True, is_file_ok=False)
+
+#         if out_dir != g4x_obj.data_dir:
+#             func_out = out_dir / func_name
+#             func_out.mkdir(parents=True, exist_ok=True)
+#             out_dir = func_out
+
+#         gap = 12
+#         click.secho(f'\nStarting: {func_name}\n', bold=True)
+#         print_k_v('sample_dir', f'{g4x_obj.data_dir}', gap)
+#         print_k_v('out_dir', f'{out_dir}', gap)
+#         print_k_v('n_threads', f'{n_threads}', gap)
+#         print_k_v('verbosity', f'{verbose}', gap)
+#         print_k_v('g4x-helpers', f'v{__version__}', gap)
+#         click.echo('')
+
+#         log_dir = g4x_obj.data_dir / 'g4x_helpers' / 'logs'
+#         log_dir.mkdir(parents=True, exist_ok=True)
+#         if not kwargs.get('logger', None):
+#             logger = utils.setup_logger(
+#                 logger_name=func_name, out_dir=log_dir, stream_level=verbose, file_logger=file_logger
+#             )
+#         logger.info(f'Running {func_name} with G4X-helpers v{__version__}')
+
+#         result = func(
+#             g4x_obj=g4x_obj,
+#             out_dir=out_dir,
+#             n_threads=n_threads,
+#             verbose=verbose,
+#             logger=logger,
+#             **kwargs,
+#         )
+
+#         click.secho(f'\nCompleted: {func.__name__}', bold=True, fg='green')
+#         return result
+
+#     return wrapper
