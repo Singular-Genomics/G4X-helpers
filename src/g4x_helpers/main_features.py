9a9f7d14dfe2a3960eafe8bcb1be8530eb80f583
floRaths <raths.f@gmail.com>
2026-04-14 17:05:11 -0700

    validate() main feature

--- src/g4x_helpers/main_features.py
+++ src/g4x_helpers/main_features.py
@@ -27,9 +27,11 @@ def _base_command(func):
         **kwargs,
     ):
         func_name = func.__name__
-        out_dir = io.pathval.validate_dir_path(out_dir)
 
-        if out_dir != smp.data_dir:
+        if out_dir is None:
+            out_dir = smp.data_dir
+        else:
+            out_dir = io.pathval.validate_dir_path(out_dir)
             func_out = out_dir / func_name
             if not func_out.exists():
                 func_out.mkdir(parents=True, exist_ok=True)
@@ -77,21 +79,6 @@ def _base_command(func):
         return result
 
     return wrapper
-
-
-@_base_command
-def testo(
-    smp: 'G4Xoutput',
-    out_dir: str,
-    compute_backend: Literal['cpu', 'gpu', 'auto'] = 'auto',
-    overwrite: bool = True,
-    **kwargs,
-):
-    log = kwargs.get('logger', LOGGER)
-    log.info('This is a test command.')
-    log.info(f'Output directory: {out_dir}')
-    log.info(f'Compute backend: {compute_backend}')
-    log.info(f'Overwrite: {overwrite}')
 
 
 @_base_command
@@ -186,80 +173,12 @@ def migrate(
 
 
 @_base_command
-def validate(
-    g4x_obj: 'G4Xoutput',
-    *,
-    logger: logging.Logger,
-    **kwargs,
-):
-    from .schemas import validate
+def validate(smp: 'G4Xoutput', **kwargs):
 
-    try:
-        validate.validate_g4x_data(
-            g4x_obj.data_dir, schema_name='base_schema', formats={'sample_id': g4x_obj.sample_id}, report='long'
-        )
-    except validate.ValidationError as e:
-        print('Directory structure validation failed!: ', e)
+    log = kwargs.get('logger', LOGGER)
+    report = smp.src.validation_report(raise_exception=False)
 
-    try:
-        print('\n')
-        validate.validate_file_schemas(g4x_obj.data_dir, verbose=True)
-    except validate.ValidationError as e:
-        print('File schema validation failed!: ', e)
-
-
-# def _base_command(func):
-#     """Decorator to apply standard command initialization logic."""
-
-#     @functools.wraps(func)
-#     def wrapper(
-#         g4x_obj: 'G4Xoutput',
-#         out_dir: str | None = None,
-#         n_threads: int = c.DEFAULT_THREADS,
-#         verbose: int = 1,
-#         file_logger: bool = True,
-#         **kwargs,
-#     ):
-#         func_name = func.__name__
-
-#         if func_name not in ('migrate', 'validate'):
-#             g4x_obj.validate()
-
-#         out_dir = g4x_obj.data_dir if out_dir is None else out_dir
-#         out_dir = utils.validate_path(out_dir, must_exist=False, is_dir_ok=True, is_file_ok=False)
-
-#         if out_dir != g4x_obj.data_dir:
-#             func_out = out_dir / func_name
-#             func_out.mkdir(parents=True, exist_ok=True)
-#             out_dir = func_out
-
-#         gap = 12
-#         click.secho(f'\nStarting: {func_name}\n', bold=True)
-#         print_k_v('sample_dir', f'{g4x_obj.data_dir}', gap)
-#         print_k_v('out_dir', f'{out_dir}', gap)
-#         print_k_v('n_threads', f'{n_threads}', gap)
-#         print_k_v('verbosity', f'{verbose}', gap)
-#         print_k_v('g4x-helpers', f'v{__version__}', gap)
-#         click.echo('')
-
-#         log_dir = g4x_obj.data_dir / 'g4x_helpers' / 'logs'
-#         log_dir.mkdir(parents=True, exist_ok=True)
-#         if not kwargs.get('logger', None):
-#             logger = utils.setup_logger(
-#                 logger_name=func_name, out_dir=log_dir, stream_level=verbose, file_logger=file_logger
-#             )
-#         logger.info(f'Running {func_name} with G4X-helpers v{__version__}')
-
-#         result = func(
-#             g4x_obj=g4x_obj,
-#             out_dir=out_dir,
-#             n_threads=n_threads,
-#             verbose=verbose,
-#             logger=logger,
-#             **kwargs,
-#         )
-
-#         click.secho(f'\nCompleted: {func.__name__}', bold=True, fg='green')
-#         return result
-
-#     return wrapper
+    if smp.src.is_valid_all:
+        logut.log_msg_wrapped('Sample is valid:\n', report, logger=log, level='info', prefix=' ')
+    else:
+        logut.log_msg_wrapped('Sample validation failed:\n', report, logger=log, level='error', prefix=' ')
